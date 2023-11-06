c2f3b8cf7091e32cf059c5b5fccb12fc39d3f6b0
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-06 05:52:24 -0800

    Header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+char *_strdup(char *str);
+
+#endif
