a83da145b4946b1f6e9c71756f049c80fcba9ad3
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-06 06:57:44 -0800

    file for string functions

--- string1.c
+++ string1.c
@@ -53,3 +53,23 @@ size_t _strlen(const char *str)
 
 	return (len); /*Return length*/
 }
+
+
+/**
+ * _strcpy - copies a string from src to dest includeing null byte
+ * @dest: destination value
+ * @src: source value
+ *
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	dest[a] = '\0';
+	return (dest);
+}
