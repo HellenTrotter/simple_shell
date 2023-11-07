e5a412b844b7459adce1afae3704a2c0f87b11ee
HellenTrotter <m.hbodiba@gmail.com>
2023-11-07 03:15:50 -0800

    Get input function

--- get_input.c
+++ get_input.c
@@ -15,7 +15,6 @@ void *get_input(void)
 	char *input_line;
 	size_t buff_size = 0;
 	ssize_t read_line;
-	char *error;
 	size_t len;
 
 	/*Read a line of input from user*/
@@ -24,30 +23,18 @@ void *get_input(void)
 	/*Check if getline encountered an error or EOF*/
 	if (read_line == -1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (input_line != NULL)
 		{
-			if (feof(stdin))
-			{
-				free(input_line);
-				exit(0);
-			}
-			else
-			{
-				perror("Error: No such file or directory");
-			}
-		}
-		else
-		{
-			perror("Error reading input");
+			free(input_line);
+			exit(0);
 		}
 
-		/*Free input_line buffer and exit with failure status*/
-		free(input_line);
+		perror("Error reading input");
 		exit(1);
 	}
 
 	/*Remove the trailing newline character, if it exists*/
-	size_t len = _strlen(input_line);
+	len = _strlen(input_line);
 
 	if (len > 0 && input_line[len - 1] == '\n')
 	{
