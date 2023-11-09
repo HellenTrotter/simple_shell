e54cd47f2c3996a477b80eb60ee71dcc24a56b4e
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-09 14:30:37 -0800

    token count function

--- token_count.c
+++ token_count.c
@@ -3,34 +3,27 @@
 /**
  * token_count - counts the number of tokens in a string based on a delimiter
  * @line: the input string to be tokenized
- * @delim: the character used to split the string int tokens
- * Return: total number tokens
+ * @delim: the character used to split the string in tokens
+ *
+ * Return: num of tokens
  */
-
 int token_count(char *line, const char *delim)
 {
-	int count = 0;
-	char *token;
-	char *line_dup;
+	int count;
+	char *copy = NULL, *token = NULL;
 
-	/* Duplicate the input string using strdup*/
-	line_dup = _strdup(line);
-
-	/*Check if strdup allocation failed*/
-	if (line_dup == NULL)
-	{
+	if (line == NULL)
 		return (-1);
-	}
-
-	/* Use strtok to count tokens*/
-	token = strtok(line, delim);
-
+	copy = _strdup(line);
+	if (copy == NULL)
+		return (-1);
+	token = strtok(copy, delim);
 	while (token != NULL)
 	{
 		count++;
 		token = strtok(NULL, delim);
 	}
+	free(copy);
 
-	free(line_dup);
 	return (count);
 }
