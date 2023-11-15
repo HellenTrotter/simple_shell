309feb76a04f5b56457296db9d3a140056fd0d2a
HellenTrotter <m.hbodiba@gmail.com>
2023-11-15 07:08:56 -0800

    Print environment function

--- printenv.c
+++ printenv.c
@@ -11,13 +11,6 @@ void print_environment(void)
 {
 	char **env_ptr = environ;
 
-	/*Check if the environment variable is accessable*/
-	if (environ == NULL)
-	{
-		perror("Error accessing environment variables");
-		return; /*Exit function if an error occurs*/
-	}
-
 	/*Itrate through the environment variables and print them*/
 	while (*env_ptr)
 	{
