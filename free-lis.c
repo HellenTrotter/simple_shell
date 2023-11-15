49fe7e5d02d3190c2a0f48b111457259386100af
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-15 05:11:50 -0800

    free-list

--- free-lis.c
+++ free-lis.c
@@ -1,13 +1,18 @@
 #include "shell.h"
 
+/**
+ * free_list - frees singly linked list
+ * @head: pointer to head node of linked list
+ *
+ */
 void free_list(Node *head)
 {
 	Node *temp;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
-		temp = head -> link;
-		free(head -> directory);
+		temp = head->link;
+		free(head->directory);
 		free(head);
 		head = temp;
 	}
