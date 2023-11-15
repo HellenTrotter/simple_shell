a68fd22340ea874eba56ccb662eb586ec0c66704
HellenTrotter <m.hbodiba@gmail.com>
2023-11-15 07:04:22 -0800

    Print environment function

--- printenv.c
+++ printenv.c
@@ -3,13 +3,22 @@
 
 /**
  * print_environment - displays environment variables
- * Return: 0 (success)
+ *
+ * Return: none
  */
 
 void print_environment(void)
 {
 	char **env_ptr = environ;
 
+	/*Check if the environment variable is accessable*/
+	if (environ == NULL)
+	{
+		perror("Error accessing environment variables");
+		return; /*Exit function if an error occurs*/
+	}
+
+	/*Itrate through the environment variables and print them*/
 	while (*env_ptr)
 	{
 		size_t len = _strlen(*env_ptr);
