885bbcfef2675994caf0035a11f50ce169c4d6b5
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-15 04:27:30 -0800

    executes function

--- execute.c
+++ execute.c
@@ -7,13 +7,23 @@
  * @cmd: name of executable file
  *
  */
-void exec(char **args, char **envp, char *cmd)
+int exec(char **args, char **envp, char *cmd)
 {
 	pid_t child;
 	int status, exec_var;
 	char *path = NULL;
+	char *error_message = NULL;
 
+	/* finds the path of the command if the coomand is not a bin executable*/
 	path = find_path(args[0]);
+	if (path == NULL)
+	{
+		error_message = err_message(args, cmd);
+		if (error_message == NULL)
+			return (-1);
+		free(error_message);
+		return (-1);
+	}
 	child = fork();
 	if (child == -1)
 	{
@@ -22,7 +32,6 @@ void exec(char **args, char **envp, char *cmd)
 		exit(EXIT_FAILURE);
 	}
 	if (child == 0)
-
 	{
 		exec_var = execve(path, args, envp);
 		if (exec_var == -1)
@@ -38,16 +47,53 @@ void exec(char **args, char **envp, char *cmd)
 		free(path);
 	}
 
+	return (0);
 }
-/*void err_message(char *name, char *cmd, char *arg)
+
+/**
+ * err_message - prints the a customized error message if command is not found
+ * @args: array of strings containing the commands entered by user
+ * @cmd: name of program
+ *
+ * Return: pointer to the error message
+ */
+char *err_message(char **args, char *cmd)
 {
-	_strcpy(name, cmd);
-	_strcat(name, ":");
-	_strcat(name, " ");
-	_strcat(name, "1");
-	_strcat(name, " ");
-	_strcat(name, arg);
-	_strcat(name, ":");
-	_strcat(name, " ");
-	_strcat(name, "not found");
-}*/
+	char *error_message = NULL;
+	ssize_t w;
+
+	error_message = malloc(sizeof(char) * 100);
+	if (error_message == NULL)
+		return (NULL);
+	full_message(error_message, args[0], cmd);
+	w = write(STDERR_FILENO, error_message, _strlen(error_message));
+	if (w == -1)
+	{
+		perror("Error ");
+		free(error_message);
+		return (NULL);
+	}
+
+	return (error_message);
+}
+
+/**
+ * full_message - used to customize the error message printed when command
+ * is not found
+ * @message: ponter to allocated space for the error message
+ * @cmd: name of the command that could not be found
+ * @arg: name of the program
+ *
+ */
+void full_message(char *message, char *cmd, char *arg)
+{
+	char *ptr = ": ";
+
+	_strcpy(message, arg);
+	_strcat(message, ptr);
+	_strcat(message, "1");
+	_strcat(message, ptr);
+	_strcat(message, cmd);
+	_strcat(message, ptr);
+	_strcat(message, "not found\n");
+}
