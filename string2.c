f959322dad1f6840c5f50bb2c1cdc3d3d52c5d53
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-15 08:52:04 -0800

    String functions

--- string2.c
+++ string2.c
@@ -46,3 +46,23 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * _memcpy - Copies memory area
+ * @dest: ponter to the meory address to be copied to
+ * @src: ponter to the memory address to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	return (dest);
+}
