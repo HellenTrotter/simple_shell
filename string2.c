4a96a3a775036f84975b5ec851267d51deff3e7c
Kossy-Augusta <kossyudogu@gmail.com>
2023-11-15 05:08:29 -0800

    string functions

--- string2.c
+++ string2.c
@@ -6,7 +6,7 @@
  * @s2: second string
  *@n: num of characters to compre
 
- * Return: An integar less than 0, greater than 0 or equal to zero if s1 is 
+ * Return: An integar less than 0, greater than 0 or equal to zero if s1 is
  * less than,greater than or equal to s2 respectively
  */
 int _strncmp(char *s1, char *s2, size_t n)
